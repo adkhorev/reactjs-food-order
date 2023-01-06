cf911b72ef68871052d0052821b64eeea456fe6c
Aleksei Khorev <adkhorev@mail.ru>
2023-01-07 02:34:16 +0300

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-import cart from './components/Cart/Cart';
 import Cart from './components/Cart/Cart';
 
 function App() {
